a5ea451cf75b912f4ed34ba39ba9890a9242dcec
Ville Vaara <villepvaara@gmail.com>
2017-10-19 14:53:13 +0300

    plot added

--- draw_plots.py
+++ draw_plots.py
@@ -1,9 +1,10 @@
 import plotly.offline as py
 import plotly.graph_objs as go
 import csv
+from collections import OrderedDict
 
 
-def draw_totals_comparison(csv_filename):
+def read_plotdata_csv(csv_filename):
     with open(csv_filename, 'r') as csvfile:
         data = []
         reader = csv.DictReader(csvfile)
@@ -12,25 +13,72 @@ def draw_totals_comparison(csv_filename):
     return data
 
 
-csv_filename = "output/data_used_for_plots/all_compared_from_others.csv"
-csvdata = draw_totals_comparison(csv_filename)
-xdata = []
-ydata = []
-for item in csvdata:
-    xdata.append(item.get('Author'))
-    ydata.append(item.get('Fragments per octavo page'))
+def draw_sources_comparison():
+    csv_filename = "output/data_used_for_plots/all_compared_from_others.csv"
+    csvdata = read_plotdata_csv(csv_filename)
+    xdata = []
+    ydata = []
+    for item in csvdata:
+        xdata.append(item.get('Author'))
+        ydata.append(item.get('Fragments per octavo page'))
 
-data = [go.Bar(
+    data = [go.Bar(
+                x=xdata,
+                y=ydata
+        )]
+
+    layout = go.Layout(
+        title='Source Fragments per Octavo Page',
+        xaxis=dict(title='Author'),
+        yaxis=dict(title='Source fragments per page')
+        )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    py.plot(fig, filename="docs/sources_in_compared.html")
+
+
+def draw_headerhits_per_author(inputfile, outputfile):
+    author_headerdata = read_plotdata_csv(inputfile)
+
+    xdata = list(author_headerdata[0].keys())
+    xdata.remove('Author')
+    ydata = OrderedDict()
+    # ydata_keys = author_headerdata.get('Author')
+    # for key in ydata_keys:
+    #     ydata[key] = []
+
+    for item in author_headerdata:
+        author = item.get('Author')
+        hits = []
+        for header_key in xdata:
+            hits.append(item.get(header_key))
+        ydata[author] = hits
+
+    data = []
+    i = 0
+    for author in ydata.keys():
+        trace = go.Bar(
             x=xdata,
-            y=ydata
-    )]
+            y=ydata.get(author),
+            name=author,
+            )
+        data.append(trace)
+        i += 1
 
-layout = go.Layout(
-    title='Source Fragments per Octavo Page',
-    xaxis=dict(title='Author'),
-    yaxis=dict(title='Source fragments per page')
-    )
+    layout = go.Layout(
+        barmode='stack',
+        title='Source Fragments per Author per Header',
+        xaxis=dict(title='Header'),
+        yaxis=dict(title='Source fragments')
+        )
 
-fig = go.Figure(data=data, layout=layout)
+    fig = go.Figure(data=data, layout=layout)
 
-py.plot(fig, filename="output/figures/sources_in_compared.html")
+    py.plot(fig, filename=outputfile)
+
+
+draw_sources_comparison()
+draw_headerhits_per_author(
+    "output/data_used_for_plots/hume7_sources_fragments_per_author_top20.csv",
+    "docs/hume7_header_authors.html")
