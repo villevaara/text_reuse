a0bf736c6764a3170ee1e5ab9efee4c1fe9abedf
Ville Vaara <villepvaara@gmail.com>
2017-10-24 16:43:20 +0300

    webplots

--- draw_plots.py
+++ draw_plots.py
@@ -13,32 +13,38 @@ def read_plotdata_csv(csv_filename):
     return data
 
 
-def draw_sources_comparison():
-    csv_filename = "output/data_used_for_plots/all_compared_from_others.csv"
-    csvdata = read_plotdata_csv(csv_filename)
+def draw_sources_comparison(inputcsv, outputfile, plot_title,
+                            save_img=False):
+    # inputcsv = "output/data_used_for_plots/all_compared_from_others.csv"
+    # outputfile = "docs/sources_in_compared.html"
+    # plot_title = 'Source Fragments per Octavo Page'
+    csvdata = read_plotdata_csv(inputcsv)
     xdata = []
     ydata = []
     for item in csvdata:
         xdata.append(item.get('Author'))
         ydata.append(item.get('Fragments per octavo page'))
-
     data = [go.Bar(
                 x=xdata,
                 y=ydata
         )]
-
     layout = go.Layout(
-        title='Source Fragments per Octavo Page',
+        title=plot_title,
         xaxis=dict(title='Author'),
         yaxis=dict(title='Source fragments per page')
         )
-
     fig = go.Figure(data=data, layout=layout)
+    py.plot(fig, filename=outputfile)
+    if save_img:
+        py.plot(fig,
+                image='png',
+                image_filename=outputfile,
+                image_width=1200,
+                image_height=600)
 
-    py.plot(fig, filename="docs/sources_in_compared.html")
 
-
-def draw_headerhits_per_author(inputfile, outputfile, plot_title):
+def draw_headerhits_per_author(inputfile, outputfile, plot_title,
+                               save_image=False):
     author_headerdata = read_plotdata_csv(inputfile)
 
     xdata = list(author_headerdata[0].keys())
@@ -78,10 +84,18 @@ def draw_headerhits_per_author(inputfile, outputfile, plot_title):
 
     fig = go.Figure(data=data, layout=layout)
 
-    py.plot(fig, filename=outputfile)
+    py.plot(fig, filename=outputfile + ".html")
+    if save_image:
+        py.plot(fig,
+                image='png',
+                image_filename=outputfile,
+                image_width=1200,
+                image_height=800,
+                )
 
 
-def draw_source_fragments_per_author(inputcsv, outputfile, plot_title):
+def draw_source_fragments_per_author(inputcsv, outputfile, plot_title,
+                                     save_image=False):
     plotdata = read_plotdata_csv(
         inputcsv)
 
@@ -118,20 +132,104 @@ def draw_source_fragments_per_author(inputcsv, outputfile, plot_title):
 
     fig = go.Figure(data=data, layout=layout)
 
-    py.plot(fig, filename=outputfile)
+    py.plot(fig,
+            filename=outputfile + ".html",
+            auto_open=False)
+    if save_image:
+        py.plot(fig,
+                image='png',
+                image_filename=outputfile,
+                image_width=1200,
+                image_height=800,
+                )
 
 
-# draw_sources_comparison()
+def draw_political_affiliation_volume(inputcsv,
+                                      outputfile,
+                                      plot_title,
+                                      save_img=False,
+                                      img_width=1400,
+                                      img_height=800):
+    plotdata = read_plotdata_csv(inputcsv)
+    pol_groups = ["Whig", "Tory", "Others"]
+
+    data = []
+    for pol_group in pol_groups:
+        xdata = []
+        ydata = []
+        for item in plotdata:
+            xdata.append(item.get('Header'))
+            ydata.append(item.get(pol_group))
+        pol_group_data = go.Bar(
+            x=xdata,
+            y=ydata,
+            name=pol_group)
+        data.append(pol_group_data)
+
+    layout = go.Layout(
+        title=plot_title,
+        xaxis=dict(title='Header'),
+        yaxis=dict(title='Fragments'),
+        barmode='group',
+        margin=go.Margin(
+            b=270),
+        hovermode='closests'
+        )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    py.plot(fig,
+            filename="docs/" + outputfile + ".html",
+            auto_open=False)
+    if save_img:
+        py.plot(fig,
+                image='png',
+                image_filename=outputfile,
+                image_width=img_width,
+                image_height=img_height,
+                )
+
+
+# draw_sources_comparison(
+#     inputcsv="output/data_used_for_plots/all_compared_from_others.csv",
+#     outputfile="docs/sources_in_compared.html",
+#     plot_title="Source Fragments per Octavo Page",
+#     save_img=True)
+
 # draw_headerhits_per_author(
 #     "output/data_used_for_plots/hume7_sources_fragments_per_author_top20.csv",
-#     "docs/hume7_header_authors.html",
+#     "docs/hume7_header_authors",
 #     "Source Fragments per Author per Header in Hume's History (vol. 7)")
-draw_source_fragments_per_author(
-    "output/data_used_for_plots/hume_author_totals_pol_top20.csv",
-    "docs/hume_top20_authors.html",
-    "Source Fragments per Author (top 21) in Hume's History")
 
-# draw source first pub year by decade
+# draw_headerhits_per_author(
+#     "output/data_used_for_plots/hume6_sources_fragments_per_author_top20.csv",
+#     "docs/hume6_header_authors",
+#     "Source Fragments per Author per Header in Hume's History (vol. 6)")
 
-# plotdata = read_plotdata_csv(
-#     "output/data_used_for_plots/first_ed_decade-fragments_hume6-8.csv")
+# draw_source_fragments_per_author(
+#     "output/data_used_for_plots/hume_author_totals_pol_top20.csv",
+#     "docs/hume_top21_authors",
+#     "Source Fragments per Author (top 21) in Hume's History")
+
+# draw_political_affiliation_volume(
+#     inputcsv="output/data_used_for_plots/hume7_sources_political.csv",
+#     outputfile="hume7_sources_political",
+#     plot_title="Hume's History vol. 7, Source political affiliation by headers",
+#     save_img=True)
+
+# draw_political_affiliation_volume(
+#     inputcsv="output/data_used_for_plots/carte4_sources_political.csv",
+#     outputfile="carte4_sources_political",
+#     plot_title="Carte's History vol. 4, Source political affiliation by headers",
+#     save_img=True,
+#     img_width=1000)
+
+draw_political_affiliation_volume(
+    inputcsv="output/data_used_for_plots/rapin11-12_pol_headers.csv",
+    outputfile="rapin11-12_sources_political",
+    plot_title=(
+        "Rapin's History (vols. 11-12)," +
+        " Source political affiliation by headers"),
+    save_img=True,
+    img_width=1200,
+    img_height=600)
